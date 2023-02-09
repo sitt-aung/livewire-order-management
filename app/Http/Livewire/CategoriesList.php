811701e099b0802f606d5d7df3418906f3a5b088
Sitt Aung <sittaung07@gmail.com>
2023-02-09 14:53:37 +0630

    Create new category modal

--- app/Http/Livewire/CategoriesList.php
+++ app/Http/Livewire/CategoriesList.php
@@ -5,10 +5,41 @@ namespace App\Http\Livewire;
 use App\Models\Category;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Str;
 
 class CategoriesList extends Component
 {
     use WithPagination;
+
+    public Category $category;
+
+    public bool $showModal = false;
+
+    protected function rules(): array
+    {
+        return [
+            'category.name' => ['required', 'string', 'min:3'],
+            'category.slug' => ['nullable', 'string'],
+        ];
+    }
+
+    public function openModal()
+    {
+        $this->showModal = true;
+        $this->category = new Category();
+    }
+
+    public function updatedCategoryName()
+    {
+        $this->category->slug = Str::slug($this->category->name);
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->category->save();
+        $this->reset('showModal');
+    }
 
     public function render()
     {
