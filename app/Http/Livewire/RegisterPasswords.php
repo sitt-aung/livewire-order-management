54aca5b2619bd8b3357e40d323e757e3f0c85bf6
Sitt Aung <sittaung07@gmail.com>
2023-02-09 14:03:01 +0630

    Generate random password

--- app/Http/Livewire/RegisterPasswords.php
+++ app/Http/Livewire/RegisterPasswords.php
@@ -6,6 +6,37 @@ use Livewire\Component;
 
 class RegisterPasswords extends Component
 {
+    public string $password = '';
+    public string $password_confirmation = '';
+
+    public function generatePassword(): void 
+    {
+        $lowercase = range('a', 'z');
+        $uppercase = range('A', 'Z');
+        $digits = range(0,9);
+        $special = ['!', '@', '#', '$', '%', '^', '*'];
+        $chars = array_merge($lowercase, $uppercase, $digits, $special);
+        $length = 12;
+        
+        do {
+            $password = array();
+ 
+            for ($i = 0; $i <= $length; $i++) {
+                $int = rand(0, count($chars) - 1);
+                $password[] = $chars[$int];
+            }
+ 
+        } while (empty(array_intersect($special, $password)));
+ 
+        $this->setPasswords(implode('', $password));
+    }
+ 
+    private function setPasswords($value): void
+    {
+        $this->password = $value;
+        $this->password_confirmation = $value;
+    }
+
     public function render()
     {
         return view('livewire.register-passwords');
