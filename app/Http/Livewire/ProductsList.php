a5d2296cc16282345e4852e379595dafeef834c3
Sitt Aung <sittaung07@gmail.com>
2023-02-10 11:15:44 +0630

    Products delete and multi-delete

--- app/Http/Livewire/ProductsList.php
+++ app/Http/Livewire/ProductsList.php
@@ -16,6 +16,8 @@ class ProductsList extends Component
 
     public array $countries = [];
 
+    public array $selected = [];
+
     public string $sortColumn = 'products.name';
 
     public string $sortDirection = 'asc';
@@ -37,10 +39,17 @@ class ProductsList extends Component
         ]
     ];
 
+    protected $listeners = ['delete', 'deleteSelected'];
+
     public function mount(): void
     {
         $this->categories = Category::pluck('name', 'id')->toArray();
         $this->countries = Country::pluck('name', 'id')->toArray();
+    }
+
+    public function getSelectedCountProperty(): int
+    {
+        return count($this->selected);
     }
 
     public function sortByColumn($column): void
@@ -51,6 +60,33 @@ class ProductsList extends Component
             $this->reset('sortDirection');
             $this->sortColumn = $column;
         }
+    }
+
+    public function deleteConfirm($method, $id = null): void
+    {
+        $this->dispatchBrowserEvent('swal:confirm', [
+            'type' => 'warning',
+            'title' => 'Are you sure?',
+            'text' => '',
+            'id' => $id,
+            'method' => $method,
+        ]);
+    }
+
+    public function delete($id): void
+    {
+        $product = Product::findOrFail($id);
+
+        $product->delete();
+    }
+
+    public function deleteSelected(): void 
+    {
+        $products = Product::whereIn('id', $this->selected)->get();
+ 
+        $products->each->delete();
+ 
+        $this->reset('selected');
     }
 
     public function render()
